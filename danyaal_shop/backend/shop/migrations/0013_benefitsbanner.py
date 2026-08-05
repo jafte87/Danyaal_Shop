06c166dfdb2b1c2f3d52963afeeb8489046a2bba
jafte <mostfaosi26@gmail.com>
2026-08-05 18:54:10 +0300

    Add BenefitsBanner model and make SellYourPhone editable in admin

---
+++ danyaal_shop/backend/shop/migrations/0013_benefitsbanner.py
@@ -0,0 +1,21 @@
+# Generated manually
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('shop', '0012_sociallinks'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='BenefitsBanner',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(default='Benefits Banner', max_length=100)),
+                ('desktop_image', models.ImageField(upload_to='banners/')),
+                ('mobile_image', models.ImageField(blank=True, null=True, upload_to='banners/')),
+                ('is_active', models.BooleanField(default=True)),
+            ],
+        ),
+    ]
